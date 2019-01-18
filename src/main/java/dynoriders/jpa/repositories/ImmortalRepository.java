0ad27695763b03472f38c2107e7e2dbc82e22af9
obionewilobi <wilfried.vandenberghe@gmail.com>
2019-01-18 06:57:11 +0100

    New JPQL query

--- src/main/java/dynoriders/jpa/repositories/ImmortalRepository.java
+++ src/main/java/dynoriders/jpa/repositories/ImmortalRepository.java
@@ -1,9 +1,14 @@
 package dynoriders.jpa.repositories;
 
 import dynoriders.jpa.entities.Immortal;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ImmortalRepository extends PagingAndSortingRepository<Immortal, Long> {
+
+    @Query("select sum(q.power) from Immortal i join Quickening q where i.id = :immortalId")
+    long getTotalQuickeningById(@Param("immortalId") long id);
 }
